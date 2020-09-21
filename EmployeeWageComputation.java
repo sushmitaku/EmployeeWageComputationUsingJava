ffef531f0d658ebc9925c9d872b5f7ca611eab1f
sushmitaku <ksushmita1999@gmail.com>
2020-09-21 23:59:22 +0530

    Added:Employee Daily Wage Calculated Using Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,27 +4,21 @@ import java.math.*;
 public class EmployeeWageComputation {
     public static void main(String args[])
     {
-	int DailyEmployeeWage, FullDayHour,HalfDayHour;
-        int maximum=1,minimum=0;
-        int present=1,absent=0;
+	int DailyEmployeeWage, FullDayHour=16,HalfDayHour=8;
+        int maximum=2,minimum=0;
 	int IsFullTime=2,IsPartTime=1;
         int randomValue=(int)(Math.random() * (maximum - minimum + 1) + minimum);
-        if(randomValue==IsFullTime){
-            System.out.println("Employee Present");
-	    FullDayHour=16;
-            System.out.println("DailyEmployeeWageForFullTime="+employeeWage(FullDayHour));
-	}
-	else if(randomValue==IsPartTime)
-        {
-            System.out.println("Employee Present");
-            HalfDayHour=8;
-            System.out.println("DailyEmployeeWageForHalfTime="+employeeWage(HalfDayHour));
-        }
-        else
+	switch (randomValue)
 	{
-            System.out.println("Employee Absent");
-	    DailyEmployeeWage = 0;
-            System.out.println("DailyEmployeeWage="+DailyEmployeeWage);
+	case 2:
+            System.out.println("DailyEmployeeWageForFullTime="+employeeWage(FullDayHour));
+		break;
+	case 1:
+            System.out.println("DailyEmployeeWageForHalfTime="+employeeWage(HalfDayHour));
+		break;
+        default:
+            System.out.println("DailyEmployeeWage=  0");
+		break;
         }
     }
 	public static int employeeWage(int hours)
