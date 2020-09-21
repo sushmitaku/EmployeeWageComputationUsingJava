bddebe24fd5d4c84fb950cbb7baba64b89c46b42
sushmitaku <ksushmita1999@gmail.com>
2020-09-21 19:59:06 +0530

    Added: Checked for Employee Present or Absent

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,1 +1,15 @@
 //"Welcome To Employee Wage Computation Program On Master Branch"
+import java.util.*;
+import java.math.*;
+public class EmployeeWageComputation {
+    public static void main(String args[])
+    {
+        int maximum=1,minimum=0;
+        int present=1,absent=0;
+        int randomValue=(int)(Math.random() * (maximum - minimum + 1) + minimum);
+        if(randomValue==present)
+            System.out.println("Employee Present");
+        else
+            System.out.println("Employee Absent");
+    }
+}
